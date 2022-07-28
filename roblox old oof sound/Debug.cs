c9442ee137fdbd9e7ac7bbe770018da5cfb87e9a
kr1mk <56652866+kr1mk@users.noreply.github.com>
2022-07-28 20:01:45 +0200

    Add files via upload

---
+++ roblox old oof sound/Debug.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace roblox_old_oof_sound
+{
+    class Debug
+    {
+        public static void Log(string message, ConsoleColor color = ConsoleColor.Gray)
+        {
+            ConsoleColor old = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = old;
+        }
+
+        public static void Success(string message)
+        {
+            Log($"[+] {message}", ConsoleColor.Green);
+        }
+
+        public static void Error(string message)
+        {
+            Log($"[-] {message}", ConsoleColor.Red);
+        }
+
+        public static void Info(string message)
+        {
+            Log($"[!] {message}", ConsoleColor.Cyan);
+        }
+
+        public static void Input(string message)
+        {
+            Log($"[?] {message}", ConsoleColor.Blue);
+        }
+
+        public static void Typewriter(string message, ConsoleColor color = ConsoleColor.Gray)
+        {
+            ConsoleColor old = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                Thread.Sleep(50);
+            }
+            Console.ForegroundColor = old;
+        }
+    }
+}
---
+++ roblox old oof sound/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Microsoft.Win32;
+
+namespace roblox_old_oof_sound
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!isRobloxInstalled())
+            {
+                Debug.Error("Roblox is not installed");
+            }
+
+            Debug.Log("This tool has been made by $Pieseł$#2035, feel free to use (even copy idc)", ConsoleColor.Blue);
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\roblox-player");
+
+            string path = key.GetValue("InstallLocation").ToString();
+
+            Debug.Typewriter("Roblox found, replacing sound...\n", ConsoleColor.Cyan);
+
+            Thread.Sleep(1000);
+
+            File.WriteAllBytes(path + @"\content\sounds\ouch.ogg", Properties.Resources.ouch);
+
+            Debug.Typewriter("Sound replaced!", ConsoleColor.Cyan);
+
+            Thread.Sleep(1000);
+            Environment.Exit(0);
+        }
+
+        private static Boolean isRobloxInstalled()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+            if (key != null)
+            {
+                foreach (RegistryKey subkey in key.GetSubKeyNames().Select(keyName => key.OpenSubKey(keyName)))
+                {
+                    string name = subkey.Name;
+                    if (name != null && name.Contains("roblox-player"))
+                    {
+                        return true;
+                    }
+                }
+                key.Close();
+            }
+
+            return false;
+        }
+    }
+}
